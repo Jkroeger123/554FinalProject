08b108f69b3adc0c9fcb236f33bb527c206f40f7
Brendan O'Connell <boconne1@stevens.edu>
2021-12-01 18:07:27 -0500

    Fixed Linking issue

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import UserProvider from "../Components/UserContext";
 import LogOutButton from "../Components/LogOutButton";
+import Link from 'next/link'; 
 
 function Dashboard() {
   return (
     <UserProvider>
       <div>
         <h1>Home- Test Staging</h1>
-        <a href="/chat">Chat</a>
+        <Link href="/chat">
+          Chat
+        </Link>
+        <br />
         <LogOutButton />
       </div>
     </UserProvider>
