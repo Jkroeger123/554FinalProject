b7ce84db59892983a0f695db145d59fe13b1da48
ronaldEstevez <restevez@stevens.edu>
2021-12-13 16:14:59 -0500

    Listing DB error handling

--- Utils/db/listings.js
+++ Utils/db/listings.js
@@ -1,21 +1,89 @@
 import db from "./index.js";
+import Ajv from "ajv";
+import ajvErrors from "ajv-errors";
+
+const ajv = new Ajv({ removeAdditional: true, allErrors: true, $data: true });
+ajvErrors(ajv);
 
 // get listings collection
 const listings = db.collection("listings");
 
-const addListing = async (image, title, description, price, madeBy, school) => {
-  // create a document for new listing
-  const listingDoc = listings.doc();
+const schema = {
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    image: {
+      type: "string",
+      nullable: false,
+      errorMessage: { type: "image must be String" },
+    },
+    title: {
+      type: "string",
+      nullable: false,
+      errorMessage: { type: "title must be String" },
+    },
+    description: {
+      type: "string",
+      nullable: false,
+      errorMessage: { type: "image must be String" },
+    },
+    price: {
+      type: "integer",
+      nullable: false,
+      errorMessage: { type: "price must be Integer" },
+    },
+    madeBy: {
+      type: "string",
+      nullable: false,
+      errorMessage: { type: "madeBy must be String" },
+    },
+    school: {
+      type: "string",
+      nullable: false,
+      errorMessage: { type: "school must be String" },
+    },
+  },
+};
 
-  // set new listing fields
-  await listingDoc.set({
+const addListing = async (image, title, description, price, madeBy, school) => {
+  const newListingData = {
     image,
     title,
     description,
     price,
     madeBy,
     school,
+  };
+
+  // validate new listing fields
+  const newListingSchema = { ...schema };
+
+  newListingSchema.required = [
+    "image",
+    "title",
+    "description",
+    "price",
+    "madeBy",
+    "school",
+  ];
+
+  const validate = ajv.compile(newListingSchema);
+
+  const valid = validate(newListingData);
+
+  if (!valid) {
+    throw new Error(validate.errors[0].message);
+  }
+
+  // create a document for new listing
+  const listingDoc = listings.doc();
+
+  // set new listing fields
+  await listingDoc.set({
+    ...newListingData,
+    // generate listing date in YYYY/MM/DD format
     uploadDate: new Date().toISOString().split("T")[0],
+    active: true,
     id: listingDoc.id,
   });
 
@@ -24,6 +92,7 @@ const addListing = async (image, title, description, price, madeBy, school) => {
 };
 
 const getListingById = async (id) => {
+  // error check id parameter
   if (!id || typeof id !== "string") {
     throw new Error("Expected an argument of type 'string' for listing id.");
   }
@@ -40,60 +109,60 @@ const getListingById = async (id) => {
   return listing.data();
 };
 
-const updateListing = async (id, updatedFields) => {
+const updateListing = async (id, updatedListingData) => {
+  // error check id parameter
   if (!id || typeof id !== "string") {
     throw new Error("Expected an argument of type 'string' for listing id.");
   }
 
+  // check that non-empty object was passed
   if (
-    !updatedFields ||
-    updatedFields.constructor.name !== "Object" ||
-    !Object.keys(updatedFields).length
+    !updatedListingData ||
+    updatedListingData.constructor.name !== "Object" ||
+    !Object.keys(updatedListingData).length
   ) {
     throw new Error(
       "Expected a non-empty argument of type 'object' for listing update."
     );
   }
 
+  // validate updated listing schema
+  const updatedListingSchema = { ...schema };
+
+  const validate = ajv.compile(updatedListingSchema);
+
+  const valid = validate(updatedListingData);
+
+  if (!valid) {
+    throw new Error(validate.errors[0].message);
+  }
+
+  // update listing date to reflect date of latest change
+  updatedListingData.uploadDate = new Date().toISOString().split("T")[0];
+
   // get listing with id from listings collection
   const listing = await listings.doc(id);
 
-  // object to hold updated listing data
-  const newListingData = {};
+  // update listing
+  await listing.update(updatedListingData);
 
-  const fields = [
-    "image",
-    "title",
-    "description",
-    "price",
-    "madeBy",
-    "uploadDate",
-    "school",
-  ];
-
-  fields.forEach((f) => {
-    if (updatedFields[f]) {
-      newListingData[f] = updatedFields[f];
-    }
-  });
-
-  if (!(await listing.get()).exists) {
-    throw new Error(`Listing with id "${id}" does not exist."`);
-  }
-
-  await listing.update(updatedFields);
-
+  // return updated listing
   return (await listing.get()).data();
 };
 
-const getListingsByUser = async (username) => {
-  if (!username || typeof username !== "string") {
+const getListingsByUser = async (username, active) => {
+  // error check username parameter
+  if (!username || typeof username !== "string" || !username.trim()) {
     throw new Error(
       "Expected an argument of type 'string' for username parameter."
     );
   }
 
-  const userListings = await listings.where("madeBy", "==", username).get();
+  // get all listings made by user
+  const userListings = await listings
+    .where("madeBy", "==", username)
+    .where("active", "==", active)
+    .get();
 
   if (userListings.empty) {
     return [];
@@ -101,6 +170,7 @@ const getListingsByUser = async (username) => {
 
   const res = [];
 
+  // produce array of listing results
   userListings.forEach((l) => {
     res.push(l.data());
   });
@@ -108,14 +178,27 @@ const getListingsByUser = async (username) => {
   return res;
 };
 
+const getActiveListingsByUser = async (username) => {
+  return await getListingsByUser(username, true);
+};
+
+const getInactiveListingsByUser = async (username) => {
+  return await getListingsByUser(username, false);
+};
+
 const getListingsBySchool = async (school) => {
-  if (!school || typeof school !== "string") {
+  // error check school parameter
+  if (!school || typeof school !== "string" || !school.trim()) {
     throw new Error(
       "Expected an argument of type 'string' for school parameter."
     );
   }
 
-  const schoolListings = await listings.where("school", "==", school).get();
+  // get all listings under 'school'
+  const schoolListings = await listings
+    .where("school", "==", school)
+    .where("active", "==", true)
+    .get();
 
   if (schoolListings.empty) {
     return [];
@@ -123,6 +206,7 @@ const getListingsBySchool = async (school) => {
 
   const res = [];
 
+  // produce array for listing results
   schoolListings.forEach((l) => {
     res.push(l.data());
   });
@@ -130,25 +214,35 @@ const getListingsBySchool = async (school) => {
   return res;
 };
 
-const removeListing = async (id) => {
-  if (!id || typeof id !== "string") {
+const deleteListing = async (id) => {
+  if (!id || typeof id !== "string" || !id.trim()) {
     throw new Error("Expected an argument of type 'string' for listing id.");
   }
 
+  // retrieve listing to remove
   const listing = await listings.doc(id);
 
+  // check if listing to be removed exists
   if (!(await listing.get()).exists) {
     throw new Error(`Listing with id "${id}" does not exist."`);
   }
 
+  // delete listing
   return await listing.delete();
+};
+
+// mark listing as inactive
+const removeListing = async (id) => {
+  return await updateListing(id, { active: false });
 };
 
 export default {
   addListing,
   getListingById,
-  getListingsByUser,
+  getActiveListingsByUser,
+  getInactiveListingsByUser,
   getListingsBySchool,
   updateListing,
+  deleteListing,
   removeListing,
 };
