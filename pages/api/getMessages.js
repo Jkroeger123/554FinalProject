9a8c20d1cb18d25171d636faeedfabc711ceae0e
Justin Kroeger <jkroeger75@gmail.com>
2021-12-13 18:52:33 -0500

    init

--- pages/api/getMessages.js
+++ pages/api/getMessages.js
@@ -32,6 +32,8 @@ export default async (req, res) => {
             if(!otherConversations) otherConversations = [];
 
             let convo = otherConversations.find(convo => convo.recepient == uid);
+            if (!convo) return res.status(200).json({...conversation, displayName, photoURL});
+
             let allMessages = [...conversation.messages, ...convo.messages];
             allMessages.sort((m1, m2) => {
                 return m1.time.toDate() - m2.time.toDate()
